1c26d390b4dee58a54f9f77fa86bcfb60ce780da
woongcheol Na <32201345@dankook.ac.kr>
2025-10-19 23:29:19 +0900

    yml 파일 설정, 라이브러리 추가 ( 이 커밋이 시작점임 )

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -38,6 +38,11 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // JWT 라이브러리 (필수)
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 }
 
 tasks.withType<Test> {
