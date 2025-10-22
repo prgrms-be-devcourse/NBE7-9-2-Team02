68148c42615cb27a692dbb697d4590bde95eac3b
Jong woo <bjw709@gmail.com>
2025-10-22 21:36:54 +0900

    feat 208 : config conflicts 문제 수정(#5)

--- backend/src/main/java/com/mysite/knitly/global/config/WebMvcConfig.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.knitly.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-    // MVC 전용 설정만 필요 시 추가
-}
